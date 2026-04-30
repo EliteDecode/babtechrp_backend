a3ce36018765814f86808b09ae20741321136189
Gospel Jonathan <gospyjo@gmail.com>
2026-04-30 02:08:08 +0100

    update: updated mailer

--- src/utils/emailUtils.ts
+++ src/utils/emailUtils.ts
@@ -8,17 +8,17 @@ interface MailOptions {
 
 const sendMail = async ({ email, subject, text }: MailOptions): Promise<void> => {
 	const transporter = nodemailer.createTransport({
-		host: 'mail.privateemail.com',
+		host: 'smtp.spacemail.com',
 		port: 465,
 		secure: true,
 		auth: {
-			user: process.env.SMTP_USER,
-			pass: process.env.SMTP_PASS
+			user: 'info@babtechrp.com',
+			pass: '123@Jkmdaj'
 		}
 	});
 
 	await transporter.sendMail({
-		from: `"BST" <${process.env.SMTP_USER}>`,
+		from: `"BST" <info@babtechrp.com>`,
 		to: email,
 		subject: subject,
 		html: text
