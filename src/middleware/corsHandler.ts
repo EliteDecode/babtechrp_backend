86cbe477f99a8b6edfbe088df20500931fd9e870
Gospel Jonathan <gospyjo@gmail.com>
2026-04-30 01:29:25 +0100

    update: fix backend all origins

--- src/middleware/corsHandler.ts
+++ src/middleware/corsHandler.ts
@@ -1,18 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function corsHandler(req: Request, res: Response, next: NextFunction) {
-	const allowedOrigins = ['http://localhost:5173', 'http://localhost:3008', 'https://babtechrp.com']; // Add your allowed origins here
-	const origin = req.header('origin');
-
-	if (allowedOrigins.includes(origin || '')) {
-		res.header('Access-Control-Allow-Origin', origin);
-	}
-
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-	res.header('Access-Control-Allow-Credentials', 'true');
 
 	if (req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
 		return res.status(200).json({});
 	}
 
